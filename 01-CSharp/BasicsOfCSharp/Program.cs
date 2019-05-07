b6fd73037194df6120de8b8be71901cdc011d24d
Daniel Coombs <danielcoombs005@yahoo.com>
2019-05-07 11:47:09 -0500

    first commit to Project 0

---
+++ 01-CSharp/BasicsOfCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿//Namespace - logical collection of libraries
+//1. Predefined name space
+using System; //parent
+
+//2. User defined namespace
+namespace BasicsOfCSharp
+{
+    //Types: Class, Structures, Enums, Interfaces, Delegates
+    class Program
+    {
+        //Type Members: Methods, Variables, Constants, Properites
+        static string name = "";
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
